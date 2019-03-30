65d09bae1dd2b38b6b27e6bae8e5e69c56f5064b
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-03-30 19:07:39 -0400

    HTML Basics

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 const static = express.static(__dirname + "../assets");
 let xss = require("xss");
 
@@ -14,13 +15,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
-app.listen(3000, () => {
+app.listen(80, () => {
 	console.log("CollegeLife is now live!");
 	console.log("Your routes will be running on http://localhost:3000");
 });
 
 app.get("/login",async(req,res)=>{
-    res.send("This is the login Page")
+    res.sendFile(path.join(__dirname, "../assets/login.html"))
 })
 
 app.get("*",async(req,res)=>{
