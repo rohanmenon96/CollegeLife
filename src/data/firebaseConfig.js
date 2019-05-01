0a169e81ff11cbba1e5101969f4893e85e20b568
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-04-30 21:30:37 -0400

    firebase completed

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -1,12 +1,12 @@
 const firebase = require('firebase');
 
 let config = {
-    apiKey: "AIzaSyA_sTy2FDJnEEOXJLInfoKJH_MGJCAHpFM",
-    authDomain: "ourblog-3b6f3.firebaseapp.com",
-    databaseURL: "https://ourblog-3b6f3.firebaseio.com",
-    projectId: "ourblog-3b6f3",
-    storageBucket: "ourblog-3b6f3.appspot.com",
-    messagingSenderId: "579554998450"
+    apiKey: "AIzaSyCA5mYbPcQOpr7eXZXTBUfvF9ffd0NU928",
+    authDomain: "collegelife-5c22a.firebaseapp.com",
+    databaseURL: "https://collegelife-5c22a.firebaseio.com",
+    projectId: "collegelife-5c22a",
+    storageBucket: "collegelife-5c22a.appspot.com",
+    messagingSenderId: "849777680702"
 }
 
 firebase.initializeApp(config);
