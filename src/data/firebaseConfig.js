155b5835283c897df105144274adbc7cab77b4f7
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-04-30 21:39:22 -0400

    removing config

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -1,12 +1,7 @@
 const firebase = require('firebase');
 
 let config = {
-    apiKey: "AIzaSyCA5mYbPcQOpr7eXZXTBUfvF9ffd0NU928",
-    authDomain: "collegelife-5c22a.firebaseapp.com",
-    databaseURL: "https://collegelife-5c22a.firebaseio.com",
-    projectId: "collegelife-5c22a",
-    storageBucket: "collegelife-5c22a.appspot.com",
-    messagingSenderId: "849777680702"
+    
 }
 
 firebase.initializeApp(config);
