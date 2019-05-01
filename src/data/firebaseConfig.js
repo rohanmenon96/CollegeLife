8e72f951566280056fff08b714d38fb1f828d330
rohanmenon96 <rohanmenon.nmims@gmail.com>
2019-04-30 22:00:22 -0400

    change of properties

--- src/data/firebaseConfig.js
+++ src/data/firebaseConfig.js
@@ -1,8 +1,13 @@
 const firebase = require('firebase');
-
-let config = {
-    
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('../src/data/config.properties');
+var config = {
+    apiKey: properties.get("apiKey"),
+    authDomain: properties.get("authDomain"),
+    databaseURL: properties.get("databaseURL"),
+    projectId: properties.get("projectId"),
+    storageBucket: properties.get("storageBucket"),
+    messagingSenderId: properties.get("messagingSenderId")
 }
-
 firebase.initializeApp(config);
 module.exports = firebase;
